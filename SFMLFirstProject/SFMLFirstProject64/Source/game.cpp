7f658287193b84b25a61c94975781cdf5d77a122
wanbo wang (Betty) <30956496+ThuningStars@users.noreply.github.com>
2023-01-13 18:16:23 -0500

    p1 finish

--- SFMLFirstProject/SFMLFirstProject64/Source/game.cpp
+++ SFMLFirstProject/SFMLFirstProject64/Source/game.cpp
@@ -2,14 +2,52 @@
 
 
 Game::Game()
-	: mWindow(sf::VideoMode(640, 480), "SFML Application")
+#pragma region Step2
+	: mWindow(sf::VideoMode(800, 600), "SFML Application", sf::Style::Close)
+	, mTexture()
+	, mBackground()
 	, mPlayer()
+	, mMusic()
+	, mFont()
+	, mIcon()
+#pragma endregion
 
 {
 
 	mPlayer.setRadius(40.0f);
 	mPlayer.setPosition(100.f, 100.f);
 	mPlayer.setFillColor(sf::Color::Cyan);
+
+#pragma region step3
+
+
+	mIcon.loadFromFile("Media/Textures/icon.png");
+	mWindow.setIcon(mIcon.getSize().x, mIcon.getSize().y, mIcon.getPixelsPtr());
+
+	//Load a sprite to display
+	if (!mTexture.loadFromFile("Media/Textures/cute_image.jpg"))
+		return;
+	mBackground.setTexture(mTexture);
+
+	if (!mFont.loadFromFile("Media/Sansation.ttf"))
+		return;
+
+
+	mText.setString("Hello SFML");
+	mText.setFont(mFont);
+	mText.setPosition(5.f, 5.f);
+	mText.setCharacterSize(50);
+	mText.setFillColor(sf::Color::Black);
+
+	//mText.setRotation(45);
+
+
+	mMusic.openFromFile("Media/Sound/nice_music.ogg");
+	//Play the music
+	mMusic.play();
+
+#pragma endregion
+
 }
 
 void Game::run()
@@ -44,9 +82,15 @@ void Game::update()
 void Game::render()
 {
 	mWindow.clear();
-	mWindow.draw(mPlayer);
+
+#pragma region Step4
+
+	mWindow.draw(mBackground);
+	//Draw the string
+	mWindow.draw(mText);
+#pragma endregion
+
+	//Update the window
 	mWindow.display();
 }
-
-
 
